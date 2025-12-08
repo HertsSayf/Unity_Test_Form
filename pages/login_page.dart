25e5d514616b85437c3cd2b7a2b69a94fabc30a8
Sayf <sb24afs@herts.ac.uk>
2025-12-08 16:57:16 +0000

    Update login_page.dart added msg and forms
    Loginpage has welcome back and forms done

--- pages/login_page.dart
+++ pages/login_page.dart
@@ -1,21 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:unity_main/components/textfield.dart';
 
 class LoginPage extends StatelessWidget {
     const LoginPage({super.key});
 
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-          backgroundColor: Theme.of(context).colorScheme.surface,
-          body: Column(
-            children: [
-              //Unity Logo
-              Image.network(
-                'https://i.postimg.cc/GtXyJgkf/Unity-Logo.png',
-                height: 120,
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body:Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //Unity Logo
+            Image.network(
+              'https://i.postimg.cc/GtXyJgkf/Unity-Logo.png',
+              height: 150,
+            ),
+
+            const SizedBox(height: 50),
+
+            //Welcome back message
+            Text(
+              "WELCOME BACK",
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.inversePrimary,
+                fontSize: 16,
               ),
-            ]
-          )
-        );
-    }
+            ),
+
+            const SizedBox(height: 25),
+
+            //Username field
+            MyTextField(
+              hintText: "Username",
+            ),
+
+            const SizedBox(height: 10),
+
+            //Password field
+             MyTextField(
+              hintText: "Password",
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
 }
