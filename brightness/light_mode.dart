8175cd903d8a3423eaee0df212c7d2045a275406
Sayf <sb24afs@herts.ac.uk>
2025-12-08 01:52:33 +0000

    Update light_mode.dart
    added a light mode theme with data

--- brightness/light_mode.dart
+++ brightness/light_mode.dart
@@ -1,1 +1,11 @@
+import 'package:flutter/material.dart';
 
+ThemeData lightMode = ThemeData(
+  colorScheme: ColorScheme.light(
+    surface: Colors.grey.shade300,   //background is depreciated i.e used surface instead
+    primary: Colors.grey.shade600,
+    secondary: Colors.grey.shade200,
+    tertiary: Colors.white,
+    inversePrimary: Colors.grey.shade900,
+  ),
+);
