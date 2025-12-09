ebbf94788689c0aa6dd2cb9bfed5ca9e4fdf764a
Dacres0 <da24aby@herts.ac.uk>
2025-12-09 11:50:39 +0000

    Correct syntax and structure in login_or_register.dart
    Fix various syntax errors and improve structure.

--- auth/login_or_register.dart
+++ auth/login_or_register.dart
@@ -1,1 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
+import '../pages/login_page.dart';
+
+class LoginOrRegister extends statefulWidget {
+  const LoginOr Register({super.key});
+
+  @override
+  state<LoginOrRegister> createState() => _LoginOrRegisterState();
+}
+
+class_LoginOrRegisterState extends State<LoginOrRegister? {
+  // intially, show login page
+  bool showLoginPage = true;
+
+  // toggle between login and register page
+  void togglePages() {
+    setStater(() {
+      showLoginPage = !showLoginPage;
+    });
+  }
+      
+  @override
+  widget build(BuildContext context) {
+   if (showLoginPage) {
+     return LoginPage();
+  }  else 
+     return RegisterPage();
+  }
+}
+}
