2596e2876d3dfea28b48e3911be430f47e50aa8b
Dacres0 <da24aby@herts.ac.uk>
2025-12-09 10:53:43 +0000

    Update my_button.dart

--- components/my_button.dart
+++ components/my_button.dart
@@ -1,1 +1,12 @@
+import 'package:flutter/material.dart';
 
+class MyButton extends statelessWidget {
+  const MyButton ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return container(
+      decoration: BoxDecoration(
+        colour: Theme.of(context).colourScheme.secondary,
+      child: Center(
+        child: Tex("Button"),
