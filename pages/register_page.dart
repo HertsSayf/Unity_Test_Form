6114826957e25212904df217b331f56325964e7b
Dacres0 <da24aby@herts.ac.uk>
2025-12-09 11:23:49 +0000

    Update register_page.dart

--- pages/register_page.dart
+++ pages/register_page.dart
@@ -1,1 +1,8 @@
+import 'package:flutter/material.dart';
 
+class RegisterPage extends statelessWidget {
+  const RegisterPage({super.key});
+
+  @override
+  widget build(BuildContext context) {
+    return 
