221d4c121c4297a368f28ce37b00fd70e4e6a4e3
Dacres0 <da24aby@herts.ac.uk>
2025-12-10 16:57:29 +0000

    Add signInWithEmailPassword method for authentication

--- auth/auth_service.dart
+++ auth/auth_service.dart
@@ -5,6 +5,14 @@ class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance; 
 
   //sign in
+  Future<UserCredential> signInWithEmailPassword(String email, password) async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(email: email, password: password, );
+      return userCredential;
+    } on FirebaseAuthException catch (e) {
+      throw Exception(e.code);
+    }
+  }
 
   //sign up
 
