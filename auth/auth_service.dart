3d0fd6cdda90be31184c25c248f0b9fe05d29eaf
Dacres0 <da24aby@herts.ac.uk>
2025-12-11 12:19:11 +0000

    Add registerWithEmailPassword method for user signup

--- auth/auth_service.dart
+++ auth/auth_service.dart
@@ -15,6 +15,14 @@ class AuthService {
   }
 
   //sign up
+  Future<UserCredential> registerWithEmailPassword(String email, String password) async {
+    try {
+      UserCredential userCredential = await _auth.createUserWithEmailAndPassword(email: email, password: password, );
+      return userCredential;
+    } on FirebaseAuthException catch (e) {
+      throw Exception(e.code);
+    }
+  }
 
   //sign out
   Future<void> signOut() async {
